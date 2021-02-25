fb63e39757cb5032ddfab75a8bc5312509a50904
Suzon Das <suzonduislm52@gmail.com>
2021-02-25 22:19:50 +0600

    vendor all

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -1,13 +1,9 @@
 <?php
 $baseDir = dirname(dirname(__FILE__));
-
 return [
     'plugins' => [
-        'AkkaCKEditor' => $baseDir . '/plugins/AkkaCKEditor/',
         'Bake' => $baseDir . '/vendor/cakephp/bake/',
         'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
-        'GoogleCharts' => $baseDir . '/plugins/GoogleCharts/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
-        'WyriHaximus/TwigView' => $baseDir . '/vendor/wyrihaximus/twig-view/',
-    ],
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+    ]
 ];
